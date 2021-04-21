e216fddf305ccfb3a690a573011ef3fe5b7e88c5
mutuajoseph <wambuajoseph756@gmail.com>
2021-04-22 00:22:23 +0300

    refactored the onclick sidebar on large screens

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -17,20 +17,20 @@ function Sidebar() {
         <>
             <div className="navbar">
                 <GiHamburgerMenu className="menu-bars" onClick={showSidebar} />
-                <Link to="/" className="logo">DUKA</Link>
+                <Link to="/" className={sidebar ? 'max-logo' : 'logo'}>DUKA</Link>
             </div>
             <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
-                <ul className="nav-menu-items" onClick={showSidebar}>
+                <ul className="nav-menu-items">
                     <li className="navbar-toggle">
                         <Link to="#" className="menu-bars">
                             <IoCloseSharp style={{marginTop: '.7rem'}} onClick={showSidebar}/>
                         </Link>
-                        <Link to="/" className="logo">DUKA</Link>
+                        <Link to="/" className={sidebar ? 'max-logo' : 'logo'}>DUKA</Link>
                     </li>
 
                     {/* map the link to be shown on the sidebar */}
                     {SidebarData.map((item, index) => (
-                        <li key={index} className={item.cName} onClick={showSidebar}>
+                        <li key={index} className={item.cName}>
                             <Link to={item.path} onClick={showSidebar}>
                                 {item.icon}
                                 <span>{item.title}</span>
